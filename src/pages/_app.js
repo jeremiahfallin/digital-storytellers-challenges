2960837455089ff5ff1dfe16e87cdfb672e92e7f
Jeremiah <jeremiah.fallin@gmail.com>
2024-06-18 07:12:39 -0700

    all

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
 import "@/styles/globals.css";
+import "@/styles/nav-final.css";
+import "@/styles/nav.css";
+import "@/styles/card-final.css";
+import "@/styles/card.css";
+import "@/styles/cards-final.css";
+import "@/styles/cards.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
